e3a585c75d3de54893b875b7be768040e6b494d2
LAB22_USER ON WS36 <l22x07w36@citulab.net>
2025-03-10 09:20:24 +0800

    Initial commit

---
+++ src/test/java/com/javier/nikolaijr/it342activity/It342ActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javier.nikolaijr.it342activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It342ActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
